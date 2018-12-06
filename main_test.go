90346370bd564b4a64886c614b16a1f46a7dd264
Giedrius Statkevičius <giedrius.statkevicius@adform.com>
2018-12-06 13:41:11 +0200

    test: fix tests
    Sometimes the tests are negative so remove the assertions because the
    checks are happening later on.
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@adform.com>

--- main_test.go
+++ main_test.go
@@ -123,10 +123,8 @@ func TestProcessLine(t *testing.T) {
 	c.ch <- nil
 	for _, k := range testCases {
 		originalName := strings.Split(k.line, " ")[0]
-		data, ok := c.samples.Load(originalName)
-		assert.Equal(t, ok, true)
-		sample, ok := data.(*graphiteSample)
-		assert.Equal(t, ok, true)
+		data, _ := c.samples.Load(originalName)
+		sample, _ := data.(*graphiteSample)
 
 		if k.willFail {
 			assert.Nil(t, sample, "Found %s", k.name)
